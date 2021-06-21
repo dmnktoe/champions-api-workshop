a7f4df0b59d34bf37b0b981ca7432528e6951b3d
Domenik Töfflinger <domenikt96@yahoo.de>
2021-06-21 17:19:40 +0200

    Update header.component.ts

--- src/app/templates/header/header.component.ts
+++ src/app/templates/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+import { ApiService } from './../../core/services/api.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  champions: any;
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
-  }
-
+    this.apiService
+      .getChampions()
+      .pipe(finalize(() => {
+      }))
+      .subscribe((champions) => {
+        console.log(champions);
+        this.champions = champions;
+      })
+  };
 }
