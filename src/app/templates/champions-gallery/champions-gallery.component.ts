ea7f405c8b70ef9ee45523b2320b06aac4e4479a
Domenik Töfflinger <domenikt96@yahoo.de>
2021-06-22 13:08:46 +0200

    create champions-gallery component and display data

---
+++ src/app/templates/champions-gallery/champions-gallery.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../core/services/api.service';
+import { Champion } from '../../core/model/Champion';
+import { finalize } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-champions-gallery',
+  templateUrl: './champions-gallery.component.html',
+  styleUrls: ['./champions-gallery.component.scss']
+})
+export class ChampionsGalleryComponent implements OnInit {
+  // @ts-ignore
+  champions: Champion[];
+  isLoading = false;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.apiService
+      .getChampions()
+      .pipe(finalize(() => {
+        this.isLoading = false;
+      }))
+      .subscribe((champions) => {
+        console.log(champions);
+        this.champions = champions;
+      })
+  }
+}
