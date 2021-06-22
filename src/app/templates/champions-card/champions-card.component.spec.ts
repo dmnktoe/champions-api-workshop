131b9c98df810bfb6cbf82dd37b70fa180691a57
Domenik Töfflinger <domenikt96@yahoo.de>
2021-06-22 13:04:40 +0200

    delete test files

--- src/app/templates/champions-card/champions-card.component.spec.ts
+++ src/app/templates/champions-card/champions-card.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { ChampionsCardComponent } from './champions-card.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('ChampionsCardComponent', () => {
+  let component: ChampionsCardComponent;
+  let fixture: ComponentFixture<ChampionsCardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ ChampionsCardComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(ChampionsCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
