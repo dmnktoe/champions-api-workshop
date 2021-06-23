9da66ae5f38385dfd24154585f4acd71545437a0
Domenik Töfflinger <domenikt96@yahoo.de>
2021-06-23 17:10:12 +0200

    remove deleted SearchComponent from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './templates/header/header.component';
-import { SearchComponent } from './templates/_shared/search/search.component';
 import { ChampionsGalleryComponent } from './templates/champions-gallery/champions-gallery.component';
 import { ChampionsCardComponent } from './templates/champions-card/champions-card.component';
 import { LoaderComponent } from './templates/_shared/loader/loader.component';
@@ -16,7 +15,6 @@ import {FormsModule} from "@angular/forms";
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchComponent,
     ChampionsGalleryComponent,
     ChampionsCardComponent,
     LoaderComponent
