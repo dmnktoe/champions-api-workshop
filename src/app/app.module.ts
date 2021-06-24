c61621aeaa2837e7c64b3629ab3c2da7489257bd
Domenik Töfflinger <domenikt96@yahoo.de>
2021-06-24 14:52:26 +0200

    clean up app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from "@angular/forms";
+
+/* Main Entry */
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+/* Shared */
 import { HeaderComponent } from './templates/header/header.component';
+import { ScrollToTopComponent } from './templates/_shared/scroll-to-top/scroll-to-top.component';
+import { LoaderComponent } from './templates/_shared/loader/loader.component';
+
+/* Champions */
+import { ChampionsComponent } from './templates/champions/champions.component';
 import { ChampionsGalleryComponent } from './templates/champions-gallery/champions-gallery.component';
 import { ChampionsCardComponent } from './templates/champions-card/champions-card.component';
-import { LoaderComponent } from './templates/_shared/loader/loader.component';
+
+/* Plugins */
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Ng2SearchPipeModule } from "ng2-search-filter";
-import {FormsModule} from "@angular/forms";
-import { ScrollToTopComponent } from './templates/_shared/scroll-to-top/scroll-to-top.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +28,8 @@ import { ScrollToTopComponent } from './templates/_shared/scroll-to-top/scroll-t
     ChampionsGalleryComponent,
     ChampionsCardComponent,
     LoaderComponent,
-    ScrollToTopComponent
+    ScrollToTopComponent,
+    ChampionsComponent
   ],
   imports: [
     BrowserModule,
