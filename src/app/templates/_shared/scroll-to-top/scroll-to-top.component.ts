3edc2990f2d656ba6cbb788ca33523e5452cc6ac
Domenik Töfflinger <domenikt96@yahoo.de>
2021-06-24 12:53:46 +0200

    add scroll-to-top component

---
+++ src/app/templates/_shared/scroll-to-top/scroll-to-top.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-scroll-to-top',
+  templateUrl: './scroll-to-top.component.html',
+  styleUrls: ['./scroll-to-top.component.scss']
+})
+export class ScrollToTopComponent implements OnInit {
+  // @ts-ignore
+  windowScrolled: boolean;
+  topPosToStartShowing = 1000;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  @HostListener('window:scroll', [])
+
+  onWindowScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.windowScrolled = true;
+    } else {
+      this.windowScrolled = false;
+    }
+  }
+
+  scrollToTop() {
+    (function smoothscroll() {
+      const currentScroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - currentScroll / 8);
+      }
+    })();
+  }
+  ngOnInit() {}
+
+}
