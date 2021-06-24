f1c1564af1c21d09062b47abf13ebda12524488a
Domenik Töfflinger <domenikt96@yahoo.de>
2021-06-24 14:52:50 +0200

    prepare app component for router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChampionsComponent } from "./templates/champions/champions.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChampionsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
